043b5ba5b97fbfa556ec5e345c75687e2e2670d5
dhythm <yuta.okada.20@gmail.com>
2025-05-13 09:58:20 +0900

    install openai

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import "dotenv/config";
+import OpenAI from "openai";
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY!;
+const client = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
 
 async function run() {
   console.log("Hello, TypeScript!");
